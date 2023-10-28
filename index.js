551f1c872f8e5055b62be9c84749da3a47d70103
jtr354 <jtr354@foxmail.com>
2023-10-28 11:25:57 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import http from "http";
+
+function createApp(port, html) {
+  http
+    .createServer((req, res) => {
+      const { url } = req;
+      if (["/", "/login"].includes(url)) {
+        res.end(fs.readFileSync(html));
+        return;
+      }
+      if (url.endsWith(".js")) {
+        res.writeHead(200, "Content-Type: text/javascript");
+        res.end(fs.readFileSync("./chat.js"));
+        return;
+      }
+      res.end("");
+    })
+    .listen(port, () => {
+      console.info(`server port at ${port}`);
+    });
+}
+
+createApp(3000, "./a.html");
+createApp(4000, "./b.html");
+createApp(5858, "./c.html");
